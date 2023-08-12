51f0aaa7e2390227fade82a81ca95ab1990e7e00
umerHP <54628763+TheUmerator@users.noreply.github.com>
2023-08-12 22:07:02 +0500

    Create divs and append them to the parent

--- gui.js
+++ gui.js
@@ -1,12 +1,14 @@
-const divBox = document.querySelector('container');
+const divBox = document.querySelector('.container');
 const gridSize = 16;
 totalBoxes = gridSize * gridSize;
 
- let boxArray=[];
-for (i = 0; i < totalBoxes; i++) 
+let cell;
+
+
+for (i = 1; i <= totalBoxes; i++) 
   {
-    boxArray[i]=document.createElement('div');
-    boxArray[i]=boxArray[i].classList.add('box');
-    console.log(boxArray[i]);
+    cell=document.createElement('div');
+    cell.classList.add('cell');
+    divBox.appendChild(cell);
+    console.log(i);
   }
-  // boxArray[i]=document.createElement(div);
